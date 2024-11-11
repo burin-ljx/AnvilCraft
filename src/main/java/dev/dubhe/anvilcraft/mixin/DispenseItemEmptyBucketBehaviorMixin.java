e1c3c65150a728fa8793216f849d323334a8f6f6
ZhuRuoLing <3489382552@qq.com>
2024-11-11 16:25:41 +0800

    修复炼药锅挤奶

--- src/main/java/dev/dubhe/anvilcraft/mixin/DispenseItemEmptyBucketBehaviorMixin.java
+++ src/main/java/dev/dubhe/anvilcraft/mixin/DispenseItemEmptyBucketBehaviorMixin.java
@@ -14,10 +14,8 @@ import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.entity.EntityTypeTest;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.AABB;
-
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -26,41 +24,37 @@ import java.util.List;
 
 @Mixin(targets = "net/minecraft/core/dispenser/DispenseItemBehavior$7")
 abstract class DispenseItemEmptyBucketBehaviorMixin extends DefaultDispenseItemBehavior {
-    @Unique private final DefaultDispenseItemBehavior anvilCraft$defaultDispenseItemBehavior =
-            new DefaultDispenseItemBehavior();
-
     @Inject(
-            method = "execute(Lnet/minecraft/core/dispenser/BlockSource;Lnet/minecraft/world/item/ItemStack;)"
-                    + "Lnet/minecraft/world/item/ItemStack;",
-            at =
-                    @At(
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/core/dispenser/DefaultDispenseItemBehavior;"
-                                    + "execute(Lnet/minecraft/core/dispenser/BlockSource;Lnet/minecraft/world/item/ItemStack;)"
-                                    + "Lnet/minecraft/world/item/ItemStack;",
-                            ordinal = 1),
-            cancellable = true)
+        method = "execute(Lnet/minecraft/core/dispenser/BlockSource;Lnet/minecraft/world/item/ItemStack;)"
+            + "Lnet/minecraft/world/item/ItemStack;",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/core/dispenser/DefaultDispenseItemBehavior;"
+                + "execute(Lnet/minecraft/core/dispenser/BlockSource;Lnet/minecraft/world/item/ItemStack;)"
+                + "Lnet/minecraft/world/item/ItemStack;",
+            ordinal = 1),
+        cancellable = true
+    )
     public void takeMilkFromCow(@NotNull BlockSource source, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
         BlockPos blockPos = source.pos().relative(source.state().getValue(DispenserBlock.FACING));
         ServerLevel level = source.level();
         ServerLevel levelAccessor = source.level();
         List<Cow> cows =
-                level.getEntities(EntityTypeTest.forClass(Cow.class), new AABB(blockPos), Entity::isAlive).stream()
-                        .toList();
+            level.getEntities(EntityTypeTest.forClass(Cow.class), new AABB(blockPos), Entity::isAlive).stream()
+                .toList();
         List<Goat> goats =
-                level.getEntities(EntityTypeTest.forClass(Goat.class), new AABB(blockPos), Entity::isAlive).stream()
-                        .toList();
+            level.getEntities(EntityTypeTest.forClass(Goat.class), new AABB(blockPos), Entity::isAlive).stream()
+                .toList();
         if (cows.isEmpty() && goats.isEmpty()) return;
         levelAccessor.gameEvent(null, GameEvent.FLUID_PICKUP, blockPos);
         Item item = Items.MILK_BUCKET;
-        stack.shrink(1);
-        if (stack.isEmpty()) {
-            cir.setReturnValue(new ItemStack(item));
-            return;
-        }
-        if (source.blockEntity().insertItem(new ItemStack(item)).getCount() < 0) {
-            this.anvilCraft$defaultDispenseItemBehavior.dispense(source, new ItemStack(item));
-        }
-        cir.setReturnValue(stack);
+        cir.setReturnValue(
+            this.consumeWithRemainder(
+                source,
+                stack,
+                new ItemStack(item)
+            )
+        );
+        cir.cancel();
     }
 }
