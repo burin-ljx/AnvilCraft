aba2a226dd79da7d09c9595b48c6407aa55bde78
ZhuRuoLing <jkl-963@live.com>
2024-11-16 20:33:13 +0800

    修正压块操作

--- src/main/java/dev/dubhe/anvilcraft/api/anvil/impl/ItemCompressBehavior.java
+++ src/main/java/dev/dubhe/anvilcraft/api/anvil/impl/ItemCompressBehavior.java
@@ -2,11 +2,15 @@ package dev.dubhe.anvilcraft.api.anvil.impl;
 
 import dev.dubhe.anvilcraft.api.anvil.AnvilBehavior;
 import dev.dubhe.anvilcraft.api.event.anvil.AnvilFallOnLandEvent;
+import dev.dubhe.anvilcraft.block.HeaterBlock;
+import dev.dubhe.anvilcraft.init.ModBlocks;
 import dev.dubhe.anvilcraft.init.ModRecipeTypes;
 import dev.dubhe.anvilcraft.util.AnvilUtil;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class ItemCompressBehavior implements AnvilBehavior {
@@ -18,6 +22,10 @@ public class ItemCompressBehavior implements AnvilBehavior {
         float fallDistance,
         AnvilFallOnLandEvent event
     ) {
+        BlockState heaterIfPossible = level.getBlockState(hitBlockPos.below());
+        if ((heaterIfPossible.is(ModBlocks.HEATER) && !heaterIfPossible.getValue(HeaterBlock.OVERLOAD))
+            || (heaterIfPossible.is(Blocks.CAMPFIRE) && !heaterIfPossible.getValue(CampfireBlock.LIT))
+        ) return false;
         return AnvilUtil.itemProcess(
             ModRecipeTypes.ITEM_COMPRESS_TYPE.get(),
             level,
