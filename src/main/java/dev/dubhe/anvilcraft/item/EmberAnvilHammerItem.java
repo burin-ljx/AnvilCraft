9e2bc3c75a05eb943a9edb8a9c7f8fce5a403e78
wdfaESfaef <2622389575@qq.com>
2024-11-25 18:29:11 +0800

    余烬工具的重铸效果

--- src/main/java/dev/dubhe/anvilcraft/item/EmberAnvilHammerItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/EmberAnvilHammerItem.java
@@ -1,19 +1,30 @@
 package dev.dubhe.anvilcraft.item;
 
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.item.component.Unbreakable;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 
-public class EmberAnvilHammerItem extends AnvilHammerItem {
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+import java.util.Map;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class EmberAnvilHammerItem extends AnvilHammerItem implements IInherentEnchantment, IFireReforging {
     /**
      * 初始化铁砧锤
      *
      * @param properties 物品属性
      */
     public EmberAnvilHammerItem(Properties properties) {
-        super(
-            properties.durability(0)
-                .component(DataComponents.UNBREAKABLE, new Unbreakable(true))
-        );
+        super(properties.fireResistant());
     }
 
     @Override
@@ -24,5 +35,31 @@ public class EmberAnvilHammerItem extends AnvilHammerItem {
     @Override
     protected float calculateFallDamageBonus(float fallDistance) {
         return Math.min(120, fallDistance * 2);
+    }
+
+    @Override
+    public void appendHoverText(
+        ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+        if (pContext.level() != null) {
+            pTooltipComponents.addAll(this.getInherentEnchantmentsTooltip(pContext.level()));
+        }
+    }
+
+    @Override
+    public Map<ResourceKey<Enchantment>, Integer> getInherentEnchantments() {
+        return Map.of(Enchantments.UNBREAKING, 5);
+    }
+
+    @Override
+    public ItemEnchantments getAllEnchantments(ItemStack stack, HolderLookup.RegistryLookup<Enchantment> lookup) {
+        ItemEnchantments.Mutable enchantments = new ItemEnchantments.Mutable(super.getAllEnchantments(stack, lookup));
+
+        for (var entry : getInherentEnchantments().entrySet()) {
+            Holder.Reference<Enchantment> holder = lookup.getOrThrow(entry.getKey());
+            enchantments.set(holder, entry.getValue());
+        }
+
+        return enchantments.toImmutable();
     }
 }
---
+++ src/main/java/dev/dubhe/anvilcraft/item/IFireReforging.java
@@ -0,0 +1,8 @@
+package dev.dubhe.anvilcraft.item;
+
+/**
+ * 火焰重铸
+ */
+public interface IFireReforging {
+
+}
