4c27e886f69595ac891a7def4c4079f3da84d2cc
ZhuRuoLing <jkl-963@live.com>
2024-11-30 20:31:36 +0800

    隔离代码

--- src/main/java/dev/dubhe/anvilcraft/client/event/ClientBlockEventListener.java
+++ src/main/java/dev/dubhe/anvilcraft/client/event/ClientBlockEventListener.java
@@ -1,4 +1,4 @@
-package dev.dubhe.anvilcraft.event;
+package dev.dubhe.anvilcraft.client.event;
 
 import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.api.hammer.IHammerChangeable;
@@ -6,7 +6,6 @@ import dev.dubhe.anvilcraft.api.hammer.IHammerRemovable;
 import dev.dubhe.anvilcraft.client.gui.screen.AnvilHammerScreen;
 import dev.dubhe.anvilcraft.item.AnvilHammerItem;
 import dev.dubhe.anvilcraft.network.HammerUsePacket;
-
 import dev.dubhe.anvilcraft.util.StateUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
@@ -20,27 +19,13 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
-
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-@EventBusSubscriber(modid = AnvilCraft.MOD_ID)
-public class BlockEventListener {
-    /**
-     * 侦听左键方块事件
-     *
-     * @param event 左键方块事件
-     */
-    @SubscribeEvent
-    public static void anvilHammerAttack(@NotNull PlayerInteractEvent.LeftClickBlock event) {
-        InteractionHand hand = event.getHand();
-        if (event.getEntity().getItemInHand(hand).getItem() instanceof AnvilHammerItem) {
-            if (!AnvilHammerItem.dropAnvil(event.getEntity(), event.getLevel(), event.getPos())) {
-                event.setCanceled(true);
-            }
-        }
-    }
+@EventBusSubscriber(Dist.CLIENT)
+public class ClientBlockEventListener {
+
 
     /**
      * 侦听右键方块事件
@@ -59,9 +44,41 @@ public class BlockEventListener {
                 ) {
                     return;
                 }
+                BlockState targetBlockState = event.getLevel().getBlockState(event.getPos());
+                if (event.getLevel().isClientSide()) {
+                    clientHandle(event, targetBlockState, hand);
+                }
                 event.setCancellationResult(InteractionResult.SUCCESS);
                 event.setCanceled(true);
             }
+        }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private static void clientHandle(PlayerInteractEvent.@NotNull RightClickBlock event, BlockState targetBlockState, InteractionHand hand) {
+        Property<?> property = AnvilHammerItem.findChangeableProperty(targetBlockState);
+        if (!event.getEntity().isShiftKeyDown()
+            && AnvilHammerItem.possibleToUseEnhancedHammerChange(targetBlockState)
+            && property != null
+        ) {
+            if (targetBlockState.getBlock() instanceof IHammerChangeable ihc
+                && ihc.checkBlockState(targetBlockState)
+                && event.getEntity().getAbilities().mayBuild
+            ) {
+                List<BlockState> possibleStates = StateUtil.findPossibleStatesForProperty(targetBlockState, property);
+                if (possibleStates.isEmpty()) {
+                    PacketDistributor.sendToServer(new HammerUsePacket(event.getPos(), hand));
+                } else {
+                    Minecraft.getInstance().setScreen(new AnvilHammerScreen(
+                        event.getPos(),
+                        targetBlockState,
+                        property,
+                        possibleStates
+                    ));
+                }
+            }
+        } else {
+            PacketDistributor.sendToServer(new HammerUsePacket(event.getPos(), hand));
         }
     }
 }
