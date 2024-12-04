bdb679d98e8778dfd00f640287475c98a0294193
YocyCraft <hanhengrui@163.com>
2024-12-04 23:49:37 +0800

    Clear unused import (close #1234)

--- src/main/java/dev/dubhe/anvilcraft/block/BlockComparatorBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/BlockComparatorBlock.java
@@ -9,7 +9,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
