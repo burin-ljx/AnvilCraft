efc074b2fa0b7f8cee0079239ca929e669ce9eaa
Gugle <administration@ztxy666.cn>
2024-12-13 16:04:35 +0800

    修改部分碰撞箱运算逻辑

--- src/main/java/dev/dubhe/anvilcraft/block/StampingPlatformBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/StampingPlatformBlock.java
@@ -34,7 +34,7 @@ public class StampingPlatformBlock extends Block implements SimpleWaterloggedBlo
         Block.box(2.0, 0.0, 2.0, 14.0, 10.0, 14.0),
         Block.box(4.0, 0.0, 0.0, 12.0, 10.0, 16.0),
         Block.box(0.0, 0.0, 4.0, 16.0, 10.0, 12.0));
-    private static final VoxelShape AABB = Shapes.join(Shapes.block(), REDUCE_AABB, BooleanOp.NOT_SAME);
+    private static final VoxelShape AABB = Shapes.join(Shapes.block(), REDUCE_AABB, BooleanOp.ONLY_FIRST);
 
     public StampingPlatformBlock(Properties properties) {
         super(properties);
