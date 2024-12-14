ca99dda8e4b8db89f4976b1d3021d459315b5f12
Gugle <administration@ztxy666.cn>
2024-12-14 17:37:11 +0800

    盔甲/武器/工具回收配方

--- src/main/java/dev/dubhe/anvilcraft/data/recipe/StampingRecipeLoader.java
+++ src/main/java/dev/dubhe/anvilcraft/data/recipe/StampingRecipeLoader.java
@@ -6,6 +6,7 @@ import dev.dubhe.anvilcraft.init.ModItems;
 import dev.dubhe.anvilcraft.recipe.ChanceItemStack;
 import dev.dubhe.anvilcraft.recipe.anvil.StampingRecipe;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -13,6 +14,7 @@ import net.minecraft.world.level.ItemLike;
 
 import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import net.neoforged.neoforge.common.Tags;
+import org.jetbrains.annotations.NotNull;
 
 public class StampingRecipeLoader {
     public static void init(RegistrateRecipeProvider provider) {
@@ -94,6 +96,50 @@ public class StampingRecipeLoader {
             .result(new ItemStack(Items.SHULKER_BOX))
             .result(new ItemStack(Items.SHULKER_BOX))
             .save(provider, AnvilCraft.of("stamping/shulker_box_from_supercritical_nesting_shulker_box"));
+
+        armor(provider, Items.CHAINMAIL_HELMET, Items.CHAIN);
+        armor(provider, Items.CHAINMAIL_CHESTPLATE, Items.CHAIN);
+        armor(provider, Items.CHAINMAIL_LEGGINGS, Items.CHAIN);
+        armor(provider, Items.CHAINMAIL_BOOTS, Items.CHAIN);
+
+        armor(provider, Items.LEATHER_HELMET, Items.LEATHER);
+        armor(provider, Items.LEATHER_CHESTPLATE, Items.LEATHER);
+        armor(provider, Items.LEATHER_LEGGINGS, Items.LEATHER);
+        armor(provider, Items.LEATHER_BOOTS, Items.LEATHER);
+        armor(provider, Items.LEATHER_HORSE_ARMOR, Items.LEATHER);
+
+        tool(provider, Items.IRON_SWORD, Items.IRON_INGOT);
+        tool(provider, Items.IRON_PICKAXE, Items.IRON_INGOT);
+        tool(provider, Items.IRON_AXE, Items.IRON_INGOT);
+        tool(provider, Items.IRON_HOE, Items.IRON_INGOT);
+        tool(provider, Items.IRON_SHOVEL, Items.IRON_INGOT);
+        armor(provider, Items.IRON_HELMET, Items.IRON_INGOT);
+        armor(provider, Items.IRON_CHESTPLATE, Items.IRON_INGOT);
+        armor(provider, Items.IRON_LEGGINGS, Items.IRON_INGOT);
+        armor(provider, Items.IRON_BOOTS, Items.IRON_INGOT);
+        armor(provider, Items.IRON_HORSE_ARMOR, Items.IRON_INGOT);
+
+        tool(provider, Items.GOLDEN_SWORD, Items.GOLD_INGOT);
+        tool(provider, Items.GOLDEN_PICKAXE, Items.GOLD_INGOT);
+        tool(provider, Items.GOLDEN_AXE, Items.GOLD_INGOT);
+        tool(provider, Items.GOLDEN_HOE, Items.GOLD_INGOT);
+        tool(provider, Items.GOLDEN_SHOVEL, Items.GOLD_INGOT);
+        armor(provider, Items.GOLDEN_HELMET, Items.GOLD_INGOT);
+        armor(provider, Items.GOLDEN_CHESTPLATE, Items.GOLD_INGOT);
+        armor(provider, Items.GOLDEN_LEGGINGS, Items.GOLD_INGOT);
+        armor(provider, Items.GOLDEN_BOOTS, Items.GOLD_INGOT);
+        armor(provider, Items.GOLDEN_HORSE_ARMOR, Items.GOLD_INGOT);
+
+        tool(provider, Items.DIAMOND_SWORD, Items.DIAMOND);
+        tool(provider, Items.DIAMOND_PICKAXE, Items.DIAMOND);
+        tool(provider, Items.DIAMOND_AXE, Items.DIAMOND);
+        tool(provider, Items.DIAMOND_HOE, Items.DIAMOND);
+        tool(provider, Items.DIAMOND_SHOVEL, Items.DIAMOND);
+        armor(provider, Items.DIAMOND_HELMET, Items.DIAMOND);
+        armor(provider, Items.DIAMOND_CHESTPLATE, Items.DIAMOND);
+        armor(provider, Items.DIAMOND_LEGGINGS, Items.DIAMOND);
+        armor(provider, Items.DIAMOND_BOOTS, Items.DIAMOND);
+        armor(provider, Items.DIAMOND_HORSE_ARMOR, Items.DIAMOND);
     }
 
     private static void stamping(RegistrateRecipeProvider provider, ItemLike input, ItemLike result, int count) {
@@ -105,5 +151,24 @@ public class StampingRecipeLoader {
 
     private static void stamping(RegistrateRecipeProvider provider, ItemLike input, ItemLike result) {
         stamping(provider, input, result, 1);
+    }
+
+    private static void tool(RegistrateRecipeProvider provider, ItemLike tool, ItemLike result) {
+        StampingRecipe.builder()
+            .requires(tool)
+            .result(ChanceItemStack.of(new ItemStack(result)).withChance(0.5f))
+            .save(provider, AnvilCraft.of("stamping/tool_%s_2_%s".formatted(getName(tool), getName(result))));
+    }
+
+    private static void armor(RegistrateRecipeProvider provider, ItemLike armor, ItemLike result) {
+        StampingRecipe.builder()
+            .requires(armor)
+            .result(ChanceItemStack.of(new ItemStack(result)).withChance(0.5f))
+            .result(ChanceItemStack.of(new ItemStack(result)).withChance(0.5f))
+            .save(provider, AnvilCraft.of("stamping/armor_%s_2_%s".formatted(getName(armor), getName(result))));
+    }
+
+    private static @NotNull String getName(@NotNull ItemLike item) {
+        return BuiltInRegistries.ITEM.getKey(item.asItem()).getPath();
     }
 }
