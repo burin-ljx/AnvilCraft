9c8afed2a2a18427d0addb3c6e49479d649e155a
YocyCraft <hanhengrui@163.com>
2024-12-18 14:54:21 +0800

    Fix conversion chance of spectral anvil

---
+++ src/main/java/dev/dubhe/anvilcraft/util/SpectralAnvilConversionUtil.java
@@ -0,0 +1,23 @@
+package dev.dubhe.anvilcraft.util;
+
+import dev.dubhe.anvilcraft.init.ModBlocks;
+import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
+import it.unimi.dsi.fastutil.objects.Object2DoubleOpenHashMap;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+public class SpectralAnvilConversionUtil {
+    private static final Object2DoubleMap<Block> SPECTRAL_ANVIL_CONVERSION_CHANCE = new Object2DoubleOpenHashMap<>();
+
+    static {
+        SPECTRAL_ANVIL_CONVERSION_CHANCE.put(Blocks.DAMAGED_ANVIL, 0.01);
+        SPECTRAL_ANVIL_CONVERSION_CHANCE.put(Blocks.CHIPPED_ANVIL, 0.02);
+        SPECTRAL_ANVIL_CONVERSION_CHANCE.put(Blocks.ANVIL, 0.03);
+        SPECTRAL_ANVIL_CONVERSION_CHANCE.put(ModBlocks.ROYAL_ANVIL.get(), 0.5);
+        SPECTRAL_ANVIL_CONVERSION_CHANCE.put(ModBlocks.EMBER_ANVIL.get(), 1.0);
+    }
+
+    public static double chance(Block block){
+        return SPECTRAL_ANVIL_CONVERSION_CHANCE.getOrDefault(block, 0.03);
+    }
+}
