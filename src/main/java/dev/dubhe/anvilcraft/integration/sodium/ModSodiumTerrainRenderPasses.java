ca733ca057c5f5de394b407a6f59afa44830e6f4
ZhuRuoLing <jkl-963@live.com>
2024-12-21 19:53:26 +0800

    Revert: 修复和embeddium与sodium的兼容性问题

--- src/main/java/dev/dubhe/anvilcraft/integration/sodium/ModSodiumTerrainRenderPasses.java
+++
@@ -1,9 +0,0 @@
-package dev.dubhe.anvilcraft.integration.sodium;
-
-
-import dev.dubhe.anvilcraft.client.init.ModRenderTypes;
-import net.caffeinemc.mods.sodium.client.render.chunk.terrain.TerrainRenderPass;
-
-public class ModSodiumTerrainRenderPasses {
-    public static final TerrainRenderPass LASER = new TerrainRenderPass(ModRenderTypes.LASER, true, true);
-}
