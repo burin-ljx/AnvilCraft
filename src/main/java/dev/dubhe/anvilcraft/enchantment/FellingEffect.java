7ddbe1f040fd6efaae7d964228e3c6d5b1ef19ef
burin <ljx_burin@outlook.com>
2025-01-02 00:25:06 +0800

    fixed bug #1419

--- src/main/java/dev/dubhe/anvilcraft/enchantment/FellingEffect.java
+++ src/main/java/dev/dubhe/anvilcraft/enchantment/FellingEffect.java
@@ -34,6 +34,7 @@ public record FellingEffect(int range) implements EnchantmentEntityEffect {
 
     @Override
     public void apply(ServerLevel level, int i, EnchantedItemInUse enchantedItemInUse, Entity entity, Vec3 vec3) {
+        if (!level.getBlockState(BlockPos.containing(vec3)).is(BlockTags.LOGS)) return;
         if (entity.isShiftKeyDown()) return;
         int max = (i * AnvilCraft.config.fellingBlockPerLevel) + 1;
         if (!(entity instanceof Player player)) return;
