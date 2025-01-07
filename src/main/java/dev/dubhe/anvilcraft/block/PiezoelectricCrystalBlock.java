d5907d654db7d63f46413ccfccac8afefb6ff98a
wdfaESfaef <2622389575@qq.com>
2025-01-07 18:03:50 +0800

    调整压电晶体产电逻辑

--- src/main/java/dev/dubhe/anvilcraft/block/PiezoelectricCrystalBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/PiezoelectricCrystalBlock.java
@@ -5,27 +5,42 @@ import dev.dubhe.anvilcraft.api.chargecollector.ChargeCollectorManager.Entry;
 import dev.dubhe.anvilcraft.api.hammer.IHammerRemovable;
 import dev.dubhe.anvilcraft.block.entity.ChargeCollectorBlockEntity;
 
+import dev.dubhe.anvilcraft.init.ModBlocks;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class PiezoelectricCrystalBlock extends Block implements IHammerRemovable {
+
+    private static final Map<Block, List<Integer>> ANVIL_TYPES = new HashMap<>();
+
+    static {
+        ANVIL_TYPES.put(ModBlocks.SPECTRAL_ANVIL.get(),List.of(1,2,3,4));
+        ANVIL_TYPES.put(ModBlocks.ROYAL_ANVIL.get(),List.of(1,2,4,8));
+        ANVIL_TYPES.put(Blocks.ANVIL,List.of(1,2,4,8));
+        ANVIL_TYPES.put(Blocks.CHIPPED_ANVIL,List.of(1,2,4,8));
+        ANVIL_TYPES.put(Blocks.DAMAGED_ANVIL,List.of(1,2,4,8));
+        ANVIL_TYPES.put(ModBlocks.EMBER_ANVIL.get(),List.of(1,2,5,12));
+    }
+
     public static VoxelShape SHAPE =
         Shapes.or(Block.box(0, 14, 0, 16, 16, 16), Block.box(2, 2, 2, 14, 14, 14), Block.box(0, 0, 0, 16, 2, 16));
 
@@ -50,11 +65,13 @@ public class PiezoelectricCrystalBlock extends Block implements IHammerRemovable
     /**
      * 被铁砧砸事件
      */
-    public void onHitByAnvil(float fallDistance, Level level, BlockPos blockPos) {
-        int distance = (int) Math.min(3, fallDistance);
-        int chargeNum = (int) Math.pow(2, distance);
+    public void onHitByAnvil(FallingBlockEntity entity, float fallDistance, Level level, BlockPos blockPos) {
+        List<Integer> chargeNums = ANVIL_TYPES.get(entity.blockState.getBlock());
+        if(chargeNums == null) return;
+        int distance = (int) Math.min(chargeNums.size() - 1, fallDistance);
+        int chargeNum = chargeNums.get(distance);
         this.charge(chargeNum, level, blockPos);
-        pressureConduction(level, blockPos, chargeNum / 2, 4);
+        pressureConduction(level, blockPos, chargeNum / 2);
     }
 
     private void charge(int chargeNum, Level level, BlockPos blockPos) {
@@ -69,14 +86,12 @@ public class PiezoelectricCrystalBlock extends Block implements IHammerRemovable
         }
     }
 
-    private void pressureConduction(Level level, BlockPos blockPos, int chargeNum, int count) {
-        int c = count - 1;
-        if (c <= 0) return;
+    private void pressureConduction(Level level, BlockPos blockPos, int chargeNum) {
         BlockPos pos = blockPos.below();
         if (level.getBlockState(pos).getBlock() instanceof PiezoelectricCrystalBlock block) {
             if (chargeNum == 0) return;
             this.charge(chargeNum, level, blockPos);
-            block.pressureConduction(level, pos, chargeNum / 2, c);
+            block.pressureConduction(level, pos, chargeNum / 2);
         }
     }
 }
