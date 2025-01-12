a36e44a4d2f25f5a5aae6196258bbaecd6613f5d
DancingSnow <1121149616@qq.com>
2025-01-12 18:16:35 +0800

    修复4层炼药锅导致的压榨配方崩溃

--- src/main/java/dev/dubhe/anvilcraft/recipe/anvil/SqueezingRecipe.java
+++ src/main/java/dev/dubhe/anvilcraft/recipe/anvil/SqueezingRecipe.java
@@ -1,5 +1,6 @@
 package dev.dubhe.anvilcraft.recipe.anvil;
 
+import dev.dubhe.anvilcraft.block.Layered4LevelCauldronBlock;
 import dev.dubhe.anvilcraft.init.ModRecipeTypes;
 import dev.dubhe.anvilcraft.recipe.anvil.builder.AbstractRecipeBuilder;
 import dev.dubhe.anvilcraft.util.CodecUtil;
@@ -73,8 +74,13 @@ public class SqueezingRecipe implements Recipe<SqueezingRecipe.Input> {
     @Override
     public boolean matches(Input input, Level level) {
         if (!input.cauldronState.is(Blocks.CAULDRON)) {
-            if (input.cauldronState.hasProperty(LayeredCauldronBlock.LEVEL)) {
+            if (input.cauldronState.getBlock() instanceof LayeredCauldronBlock) {
                 if (input.cauldronState.getValue(LayeredCauldronBlock.LEVEL) >= 3) {
+                    return false;
+                }
+            }
+            if (input.cauldronState.getBlock() instanceof Layered4LevelCauldronBlock) {
+                if (input.cauldronState.getValue(Layered4LevelCauldronBlock.LEVEL) >= 4) {
                     return false;
                 }
             } else {
