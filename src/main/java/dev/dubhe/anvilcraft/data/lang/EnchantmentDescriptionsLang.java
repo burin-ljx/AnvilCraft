482b1fc7b58738ae31f12ebe11424346918355b3
YocyCraft <hanhengrui@163.com>
2025-01-18 21:31:32 +0800

    Fix EnchantmentDescriptionsLang

--- src/main/java/dev/dubhe/anvilcraft/data/lang/EnchantmentDescriptionsLang.java
+++ src/main/java/dev/dubhe/anvilcraft/data/lang/EnchantmentDescriptionsLang.java
@@ -8,8 +8,11 @@ public class EnchantmentDescriptionsLang {
      * @param provider 提供器
      */
     public static void init(@NotNull RegistrateLangProvider provider) {
-        provider.add("enchantment.anvilcraft.harvest.desc", "Chain collection of logs and their variants, with a default configuration of enchantment level n, can chain collect up to 1+2n blocks at a time.");
-        provider.add("enchantment.anvilcraft.felling.desc", "Harvest mature crops within a certain range and replant them. The first level range is 3 * 3, the second level is 5 * 5, and the third level is 7 * 7, expanding one circle per level.");
-        provider.add("enchantment.anvilcraft.beheading.desc", "Enhance the probability of the Wither Skeleton Skull dropping and cause mobs (including players) that were previously unable to drop their heads to do so.");
+        provider.add("enchantment.anvilcraft.felling.desc",
+            "Vein mining of logs and their variants, higher level increases number limit.");
+        provider.add("enchantment.anvilcraft.harvest.desc",
+            "Harvest and replant mature crops, higher level increases range.");
+        provider.add("enchantment.anvilcraft.beheading.desc",
+            "Increase drop chance of Wither Skeleton Skull and make other mobs drop their head.");
     }
 }
